f9f90e26d53782a454ac9eef7fa8f6023cc2d08f
Roman Nekrasov <roman.olbgroup@gmail.com>
2022-03-17 13:17:20 +0200

    Create components Header, Layout and Footer.

--- homework-1/src/App.js
+++ homework-1/src/App.js
@@ -1,17 +1,34 @@
-import logo from './logo.svg';
-import './App.css';
+import Header from "./components/Header/Header";
+import Layout from "./components/Layout/Layout";
+import Footer from "./components/Footer/Footer";
+import bg1 from './assets/bg1.jpg'
+
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Hello, world!
-        </p>
-      </header>
-    </div>
-  );
+	return (
+		<>
+			<Header
+				title="This is title!"
+				descr="This is description!"
+			/>
+			<Layout
+				title="Some title 1"
+				descr="Some description 1"
+				urlBg={bg1}
+			/>
+			<Layout
+				title="Some title 2"
+				descr="Some description 2"
+				colorBg="#826ecc"
+			/>
+			<Layout
+				title="Some title 3"
+				descr="Some description 3"
+				urlBg={bg1}
+			/>
+			<Footer />
+		</>
+	);
 }
 
 export default App;
---
+++ homework-1/src/components/Layout/Layout.js
@@ -0,0 +1,31 @@
+import style from './style.module.css'
+
+const Layout = ({title, descr, urlBg, colorBg}) => {
+	const layoutStyle = {}
+	if(urlBg) {
+		layoutStyle["background"] = `url(${urlBg})`
+	}
+	if(colorBg) {
+		layoutStyle["backgroundColor"] = colorBg
+	}
+
+	return (
+		<>
+			<section style={layoutStyle} className={style.root}>
+				<div className={style.wrapper}>
+					<article>
+						<div className={style.title}>
+							<h3>{title}</h3>
+							<span className={style.separator}></span>
+						</div>
+						<div className={`${style.desc} ${style.full}`}>
+							<p>{descr}</p>
+						</div>
+					</article>
+				</div>
+			</section>
+		</>
+	)
+}
+
+export default Layout
